cb9c714dcf92d028e5fe1400e828d5b4bab0ad1a
persiawinters <persia.winters.student@madisonpsb.org>
2022-02-09 20:29:37 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,19 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+var circle = ;
+var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+ physikz.addRandomVelocity(circle, canvas);
+ view.addChild(circle);
+ circles.push(circle);
+        }
 
         // TODO 3 / 8 : Call the drawCircle() function 
-
+drawCircle(5);
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
